faca60f3e56ed2c60cb2e7cc5ed1cd8f1666a4d5
JeonB <newjeonb@gmail.com>
2023-11-22 20:11:12 +0900

    Complete Mongodb refilca test  && monstache connection

---
+++ mongo/init-replicaSet.sh
@@ -0,0 +1,15 @@
+set -e
+
+mongosh --quiet --norc "mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}" --eval <<EOF
+    try {
+        rs.initiate({
+            _id: '${MONGO_REPLSET_NAME}',
+            members: [
+                { _id: 0, host: '${MONGO_HOSTNAME}' }
+            ]
+        }, { force: true });
+    } catch (e) {
+    } finally {
+        rs.status().ok;
+    }
+EOF
---
+++ mongo/initdb.d/init-mongo.sh
@@ -0,0 +1,14 @@
+set -e
+
+mongosh <<EOF
+    use admin;
+
+    db.createUser({
+        user: '${MONGO_INITDB_USER}',
+        pwd: '${MONGO_INITDB_PWD}',
+        roles: [{
+            role: 'readWrite',
+            db: 'admin'
+        }]
+    })
+EOF
