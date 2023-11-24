6270b98515fa47bedddb8bbffc9c59270a2a6f18
JeonB <newjeonb@gmail.com>
2023-11-24 18:02:13 +0900

    Mod mongo sh

--- mongo/init-replicaSet.sh
+++ mongo/init-replicaSet.sh
@@ -1,11 +1,11 @@
 set -e
 
-mongosh --quiet --norc "mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}" --eval <<EOF
+mongosh --quiet --norc "mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}" --eval <<EOF
     try {
         rs.initiate({
             _id: '${MONGO_REPLSET_NAME}',
             members: [
-                { _id: 0, host: '${MONGO_HOSTNAME}' }
+                { _id: 0, host: '${MONGO_HOSTNAME}' , priority: 2}
             ]
         }, { force: true });
     } catch (e) {
